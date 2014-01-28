0c381731cb782367fb4094ca32df4f0ed51b45eb
Julien Dreux <julien.dreux@gmail.com>
2014-01-28 02:48:29 -0500

    issues with the file download

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var express = require('express'),
 	path = require('path'),
 	http = require('http'),
+	https = require('https'),
+	fs = require('fs'),
 	async = require('async'),
 	_ = require('lodash'),
 	exphbs  = require('express3-handlebars'),
+	rimraf = require('rimraf'),
+	request = require('request'),
 	Dropbox = require("dropbox"),
 	client = new Dropbox.Client({
 	    key: "do4maj0zi41t4jb",
@@ -121,6 +125,139 @@ app.get('/dropbox/albums', function(req, res, next){
 });
 
 app.get('/', function(req, res, next){
+
+	//Clean up:
+
+	// rimraf('/public/')
+
+	var albums = [];
+
+	client.readdir('/website/albums/', function(error, folders){
+
+		if(error) {
+			next(error);
+		} else {
+			// albums = _.map(folders, function(a){
+			// 	return {
+			// 		name: a,
+			// 		art: []
+			// 	}
+			// });
+			console.log("Got folders:", folders);
+			async.map(folders, function(album, cb){
+				client.readdir('/website/albums/'+album, function(error, files){
+					if(error) return cb(error);
+					cb(null, {
+						name: album,
+						art: _.map(files, function(filename){
+							var src = '/website/albums/'+album+'/'+filename,
+								name = path.basename(src, path.extname(src));
+							return {
+								name: name,
+								thumbURL: client.thumbnailUrl(src, {size: 'medium'}),
+								src: src,
+								dest: './public/dist/'+album+'/'+filename,
+								thumbDest: './public/dist/'+album+'/'+name+'.thumbnail'+path.extname(src)
+							}
+						})
+					});
+				});
+			}, function(err, albums){
+				if(err) next(err);
+
+				//Remove old dist content, create new albums.
+				async.series([
+					_.partial(rimraf, './public/dist/'),
+					_.partial(fs.mkdir, './public/dist/'),
+				].concat(_.map(albums, function(a){
+					return _.partial(fs.mkdir, './public/dist/'+a.name);
+				})), function(err){
+					if(err) return next(err);
+
+					// Generate the list of the files to download (pic + thumbnail)
+
+					function downloadThumb(url, dest, cb){
+						console.log("thumb:",url,dest);
+						var file = fs.createWriteStream(dest);
+						request(url).pipe(file);
+						file.on('finish', function() {
+							console.log("finish!");
+					    	file.close();
+					    	cb();
+					    });
+					}
+
+					function downloadPic(src, dest, cb) {
+						console.log("Pic:",src,dest);
+						client.readFile(src, function(error, data) {
+							console.log("Pic error:",src,dest,error);
+							if(error) return cb(error);
+						  	fs.writeFile(dest, data, function(error) {
+							    if(error) return cb(error);
+							    console.log("Created "+dest);
+							    cb();
+							}); 
+						});
+					}
+
+					var dl = _.reduce(albums, function(acc, a){
+						
+
+						var thumbs = _.map(a.art, function(p){
+							return _.partial(downloadThumb, p.thumbURL, p.thumbDest);
+						});
+
+						var pics = _.map(a.art, function(p){
+							return _.partial(downloadPic, p.src, p.dest);
+						});
+
+						console.log("partials", thumbs, pics);
+
+						return acc.concat(thumbs).concat(pics);
+
+						// acc.concat();
+						// acc.concat(_.map(a.art, function(p){
+						// 	return _.partial(downloadPic, p.src, p.dest);
+						// }));
+						// console.log(acc);
+						// return acc;
+					}, []);
+
+					//Fire in the hole
+					console.log("Downloading: "+dl.length+" files.");
+					async.series(dl, function(error){
+						if(error) return next(error);
+						res.json(albums);
+					});
+
+					// console.log(dl);
+
+					// _.each(albums, function(a){
+					// 	_.each(a.art, function(p){
+					// 		 var thumb = fs.createWriteStream(p.localThumbPath);
+					// 		 var request = http.get(url, function(response) {
+					// 		    response.pipe(file);
+					// 		    file.on('finish', function() {
+					// 		      file.close();
+					// 		      cb();
+					// 		    });
+					// 		  });
+					// 	});
+					// })
+
+					//All the albums folders are created, download the files
+					// async.parallel() 
+				});
+			});
+		};
+	});	
+});
+
+app.get('/old', function(req, res, next){
+
+	//Clean up:
+
+	// rimraf('/public/')
 
 	var albums = [];
 
