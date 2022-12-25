92ec7fae3d904217f176b0ea4f6f8efe34544cef
Julien Dreux <juliend@meta.com>
2022-12-25 17:57:51 +0100

    more agressive unveil logic

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,7 @@
 $(function(){
     $('#blueimp-gallery').data('useBootstrapModal', false);
     $('#blueimp-gallery').toggleClass('blueimp-gallery-controls', true);
-    $("img").unveil();
+    $("img").unveil(4000);
     $("#menu").sticky({
       topSpacing:0,
       zIndex:100
